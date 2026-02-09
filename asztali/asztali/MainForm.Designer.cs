98a9294ab0c785d7880790cc78747f48dad93e7d
Szaszik-Adrian <szaszik.adrian@gmail.com>
2026-02-09 10:48:27 +0100

    gombok véglegesítés

---
+++ asztali/asztali/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace asztali
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvFilmek = new DataGridView();
+            btnLoadFilmek = new Button();
+            label1 = new Label();
+            btnBack = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvFilmek).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvFilmek
+            // 
+            dgvFilmek.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvFilmek.Location = new Point(27, 24);
+            dgvFilmek.Name = "dgvFilmek";
+            dgvFilmek.Size = new Size(150, 111);
+            dgvFilmek.TabIndex = 0;
+            // 
+            // btnLoadFilmek
+            // 
+            btnLoadFilmek.Location = new Point(230, 20);
+            btnLoadFilmek.Name = "btnLoadFilmek";
+            btnLoadFilmek.Size = new Size(180, 147);
+            btnLoadFilmek.TabIndex = 1;
+            btnLoadFilmek.Text = "Filmek betöltése";
+            btnLoadFilmek.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(412, 386);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 2;
+            label1.Text = "MainForm";
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(39, 281);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(207, 122);
+            btnBack.TabIndex = 3;
+            btnBack.Text = "Vissza";
+            btnBack.UseVisualStyleBackColor = true;
+            btnBack.Click += this.btnBack_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnBack);
+            Controls.Add(label1);
+            Controls.Add(btnLoadFilmek);
+            Controls.Add(dgvFilmek);
+            Name = "MainForm";
+            Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)dgvFilmek).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvFilmek;
+        private Button btnLoadFilmek;
+        private Label label1;
+        private Button btnBack;
+    }
+}
---
+++ asztali/asztali/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asztali
+{
+    public partial class MainForm : Form
+    {
+        private readonly ApiClient _api;
+
+        public MainForm() : this(new ApiClient("http://localhost:3000"))
+        {
+        }
+
+        public MainForm(ApiClient api)
+        {
+            InitializeComponent();
+            _api = api;
+        }
+
+        private async void btnLoadFilmek_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var filmek = await _api.GetAsync<List<FilmDto>>("/filmek/");
+                dgvFilmek.AutoGenerateColumns = true;
+                dgvFilmek.DataSource = filmek;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Filmek betöltése hiba: " + ex.Message);
+            }
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();  // NEM Close!
+
+            var login = new LoginForm();
+
+            // ha a login bezáródik, a main visszajön
+            login.FormClosed += (s, args) =>
+            {
+                this.Show();
+            };
+
+            login.Show();
+        }
+
+    }
+
+}
